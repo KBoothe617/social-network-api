b28fe003228f779569f1ddbcf9d1e0237b148422
Kaleb Boothe <kboothe617@gmail.com>
2024-12-10 19:08:02 -0700

    userController.js create and update user

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,4 +17,16 @@ module.exports = {
         .then(user => user ? res.json(user) : res.status(404).json({ message: 'No user found with this id!' }))
         .catch(err => res.status(500).json(err));
     },
+    // create a new user
+    createUser(req, res) {
+        User.create(req.body)
+        .then(user => res.json(user))
+        .catch(err => res.status(500).json(err));
+    },
+    // update user by id
+    updateUser(req, res) {
+        User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        .then(user => user ? res.json(user) : res.status(404).json({ message: 'No user found with this id!' }))
+        .catch(err => res.status(500).json(err));
+    },
 };
