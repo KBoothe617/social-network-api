a403db1c0aee450fb4e973f2f0a085af65c0d610
Kaleb Boothe <kboothe617@gmail.com>
2024-12-10 18:40:26 -0700

    thoughts.js create thought schema

--- models/thoughts.js
+++ models/thoughts.js
@@ -24,3 +24,26 @@ const reactionsSchema = new Schema({
 }, {
     toJSON: { getters: true },
 });
+
+// create thought schema
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: timestamp => new Date(timestamp).toLocaleString(),
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [reactionsSchema]
+}, {
+    toJSON: { virtuals: true },
+    id: false,
+});
