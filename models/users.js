b41a0cd2e1c043a0a1c61e514351d09285342b22
Kaleb Boothe <kboothe617@gmail.com>
2024-12-10 18:28:42 -0700

    users.js schema set up

---
+++ models/users.js
@@ -0,0 +1,32 @@
+// import schem and model from mongoose
+import { Schema, model } from 'mongoose';
+
+// create a new schema
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought',
+        },
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        },
+    ],
+    toJSON: { virtuals: true },
+    id: false,
+});
