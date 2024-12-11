2224127eb14027dc1643fe90786c46c88257d1dc
Kaleb Boothe <kboothe617@gmail.com>
2024-12-10 19:05:58 -0700

    userController.js get single user by id

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,4 +9,12 @@ module.exports = {
         .then(users => res.json(users))
         .catch(err => res.status(500).json(err));
     },
+    // get single user by id
+    getUserByID(req, res) {
+        User.findById(req.params.id)
+        .populate('thoughts')
+        .populate('friends')
+        .then(user => user ? res.json(user) : res.status(404).json({ message: 'No user found with this id!' }))
+        .catch(err => res.status(500).json(err));
+    },
 };
