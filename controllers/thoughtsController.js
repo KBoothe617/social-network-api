02c99d8d332fdf9bdf3afb06c472abe6e92fcce4
Kaleb Boothe <kboothe617@gmail.com>
2024-12-10 19:24:28 -0700

    thoughtsController.js update and delete thoughts

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -27,4 +27,21 @@
         .then(user => user ? res.json(user) : res.status(404).json({ message: 'No user found with this id' }))
         .catch(err => res.status(500).json(err));
     },
+    // update thought
+    updateThought(req, res) {
+        Thought.findByIdAndUpdate(req.params.thoughtId)
+        .then(thought => thought ? res.json(thought) : res.status(404).json({ message: 'No thought found with this id' }))
+        .catch(err => res.status(500).json(err));
+    },
+    // delete thought
+    deleteThought(req, res) {
+        Thought.findByIdAndDelete(req.params.thoughtId)
+        .then(thought => thought ? User.findByIdAndUpdate(
+            thought.userId,
+            { $pull : { thoughts: thought._id } },
+            { new: true }
+        ) : res.status(404).json({ message: 'No thought found with this id' }))
+        .then(user => user ? res.json(user) : res.status(404).json({ message: 'No user found with this id' }))
+        .catch(err => res.status(500).json(err));
+    },
 };
