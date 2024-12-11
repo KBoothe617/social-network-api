0314fee63b355a1a4e94561efa34b1860cdd9d25
Kaleb Boothe <kboothe617@gmail.com>
2024-12-10 19:21:39 -0700

    toughtsController.js create thought

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -8,4 +8,23 @@
         .then(thought => thought ? res.json(thought) : res.status(404).json({ message: 'No thoughts found' }))
         .catch(err => res.status(500).json(err));
     },
+    // get thought by id
+    getThoughtsById(req, res) {
+        Thought.findById(req.params.id)
+        .then(thought => thought ? res.json(thought) : res.status(404).json({ message: 'No thought found with this id' }))
+        .catch(err => res.status(500).json(err));
+    },
+    // create thought
+    createThought(req, res) {
+        Thought.create(req.body)
+        .then(thought => {
+            return User.findByIdAndUpdate(
+                req.body.userId,
+                { $addToSet : { thoughts: thought._id } },
+                { new: true }
+            );
+        })
+        .then(user => user ? res.json(user) : res.status(404).json({ message: 'No user found with this id' }))
+        .catch(err => res.status(500).json(err));
+    },
 };
